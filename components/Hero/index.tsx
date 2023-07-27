8d8e24fe4a0d767b670e06db0e2fd536452646cc
Seundede <omadoyedede0@gmail.com>
2023-07-27 21:48:57 +0100

    feat: hero section of the application

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,14 +1,15 @@
 "use client";
 import React from "react";
 import CustomButton from "../CustomButton";
+import Image from "next/image";
 
 const handleScroll = () => {};
 
 const Hero = () => {
   return (
-    <section className="flex flex-col lg:flex-row lg:justify-between gap-[20px] container mx-auto">
-      <div className="pt-[60px] px-[10px] lg:px-[20px]">
-        <h2 className="font-extrabold text-[40px] lg:text-[70px]">
+    <section className="flex flex-col lg:flex-row justify-between items-center px-[20px] lg:px-[40px] ">
+      <div className="pt-[60px] ">
+        <h2 className="font-bold text-[40px] lg:text-[70px]">
           Premium Car Rental in Nigeria
         </h2>
         <p className="font-light mt-5 text-[20px] lg:text-[50px]">
@@ -21,7 +22,9 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-      <div className="pt-[60px]">image</div>
+      <div className="pt-[60px]">
+        <Image src="/hero.jpg" alt="hero image" width={1500} height={1500} />
+      </div>
     </section>
   );
 };
